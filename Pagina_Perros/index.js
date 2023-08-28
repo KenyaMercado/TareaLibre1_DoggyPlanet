745ace3a89d6feb81e55c0e71cbe337b40b83698
KenyaMercado <142718861+KenyaMercado@users.noreply.github.com>
2023-08-27 23:37:30 -0600

    Delete Pagina_Perros directory

--- Pagina_Perros/index.js
+++
@@ -1,9 +0,0 @@
-const btnNombre = document.getElementById('btn-nombre');
-const nombrePerroInput = document.getElementById('nombre-perro');
-
-btnNombre.addEventListener('click', () => {
-    const nombrePerro = nombrePerroInput.value;
-    if (nombrePerro.trim() !== '') {
-        window.location.href = `lugares.html?nombre=${encodeURIComponent(nombrePerro)}`;
-    }
-});
